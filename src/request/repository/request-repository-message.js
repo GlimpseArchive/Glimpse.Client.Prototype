4fbf99c63d49d2f0eeb0636d4d0e3098397854d4
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-05-19 13:59:34 -0700

    Update message processing to pull out abstract

--- src/request/repository/request-repository-message.js
+++ src/request/repository/request-repository-message.js
@@ -4,9 +4,18 @@ var glimpse = require('glimpse');
 
 var create = (function() {
     var processMessageSummary = function(request, message) {
+        // process indexes (promoted to first class properties)
         if (message.indexes && message.indexes.length > 0) {
             for (var index in message.indexes) {
                 request[index] = message.indexes[index];
+            }
+        }
+
+        // process abstract (placed in abstract property)
+        if (message.abstract) {
+            var abstract = request.abstract || (request.abstract = {});
+            for (var abstractKey in message.abstract) {
+                abstract[abstractKey] = message.abstract[abstractKey];
             }
         }
     };
