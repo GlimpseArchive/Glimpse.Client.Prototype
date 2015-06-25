6a49a92380b87b3b2041ffa481a65f2915011331
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-06-24 17:51:26 -0700

    Have temp message tab work if no tabs are present

--- src/request/repository/converter/request-converter-strategy-tab-temp.js
+++ src/request/repository/converter/request-converter-strategy-tab-temp.js
@@ -3,17 +3,16 @@
 module.exports = function(request, message) {
     var didUpdate = false; 
     
-	// TODO: Really bad hack to get messages showing in the UI 
-    if (request.tabs) {     
-    	var tabType = 'tab.messages';
-    	if (!request.tabs[tabType]) {
-        	didUpdate = true; 
-    	    request.tabs[tabType] = {
-    	            title: 'Messages',
-    	            payload: request.messages
-    	        };
-    	}
-    }
+	// TODO: Really bad hack to get messages showing in the UI  
+	var tabType = 'tab.messages';
+	if (!request.tabs) {
+    	didUpdate = true; 
+        request.tabs = {};
+	    request.tabs[tabType] = {
+	            title: 'Messages',
+	            payload: request.messages
+	        };
+	} 
     
     return didUpdate;
 };
