fcd02ad1cbc90546a027fe87461e4960d401393c
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-07-25 22:18:27 -0700

    Stub implementation for socket

--- src/request/repository/message-repository-stream.js
+++ src/request/repository/message-repository-stream.js
@@ -1,24 +1,42 @@
 'use strict';
 
 var glimpse = require('glimpse');
+var $ = require('lib/modules/jquery-signalr.js');
+
+var socket = (function() {
+    var connection;
+
+    var setup = function() {
+        connection = $.hubConnection("/Glimpse/Data/Stream", { useDefaultPath: false });
+        
+        var messageProxy = connection.createHubProxy('messageClientChannelSender');
+        messageProxy.on('summaryMessage', function(message) {  
+            glimpse.emit('data.message.summary.found.stream', [ message ]);
+        });
+        messageProxy.on('detailMessage', function(message) {  
+            glimpse.emit('data.message.detail.found.stream', [ message ]);
+        });
+        
+        connection.start({ withCredentials: false })
+            .done(function() { console.log('Now connected, connection ID=' + connection.id); })
+            .fail(function() { console.log('Could not connect'); });
+    }; 
+    
+    return {
+        check: function() {
+            if (!connection) {
+                setup();
+            }
+        }
+    };
+})();
 
 module.exports = {
     subscribeToSummariesLastest: function () {
-        // TODO: Need to complete
-        //       Subscribe via socket
-
-        // simulate success callback
-        setTimeout(function () {
-            glimpse.emit('data.message.summary.found.stream', []);
-        }, 0);
+        socket.check();
     },
     subscribeToDetailsFor: function (requestId) {
-        // TODO: Need to complete
-        //       Subscribe via socket
-
-        // simulate success callback
-        setTimeout(function () {
-            glimpse.emit('data.message.detail.found.stream', []);
-        }, 0);
+        // TODO: need to do something about switching feeds, not sure yet
+        socket.check();
     }
 };
