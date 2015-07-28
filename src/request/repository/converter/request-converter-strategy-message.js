818670d130c092f822d0edf80a61e20ef220f68e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-07-28 16:46:44 -0700

    Make sure the payload is treated as being a string coming from the server

--- src/request/repository/converter/request-converter-strategy-message.js
+++ src/request/repository/converter/request-converter-strategy-message.js
@@ -4,6 +4,11 @@ module.exports = function(request, message) {
     var didUpdate = false; 
     
     if (!request.messages[message.id]) {
+        
+        // ***** TEMP, not sure this should be at this place ****
+        message.payload = JSON.parse(message.payload);
+        // ***** TEMP, not sure this should be at this place ****
+        
         request.messages[message.id] = message; 
         didUpdate = true;
     } 
