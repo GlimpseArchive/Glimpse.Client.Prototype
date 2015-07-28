b4fa7277301987fd5b4af8295023d15426c5a936
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-07-28 10:54:09 -0700

    Fix up moment bringing in all langs

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,7 @@ module.exports = {
         new webpack.ProvidePlugin({
             'window.jQuery': require.resolve('jquery/src/core.js'),
         }),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
         progressPlugin
     ],
     log: {
