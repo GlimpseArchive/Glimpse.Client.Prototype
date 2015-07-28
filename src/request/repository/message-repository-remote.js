9541950c0a358494e5a1930978c137690c24ed6e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-07-28 12:26:54 -0700

    Update endpoint that is targeted

--- src/request/repository/message-repository-remote.js
+++ src/request/repository/message-repository-remote.js
@@ -6,8 +6,9 @@ var glimpse = require('glimpse');
 module.exports = {
     triggerGetLastestSummaries: function () {
         request
-            .get('/glimpse/api/messages') 
-            .query({ latest: true })
+            //.get('/glimpse/api/messages') 
+            //.query({ latest: true })
+            .get('/Glimpse/Data/History')  //TODO: this will probably change in time to the above
             .set('Accept', 'application/json')
             .end(function(err, res){
                 if (err.status == 200) {
