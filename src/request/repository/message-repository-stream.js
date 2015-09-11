7e80f8c40b1d14cdb4fb248a3ffba48d6d323d67
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-09-10 17:37:09 -0700

    Implementation of Server Sent Events / EventSource

--- src/request/repository/message-repository-stream.js
+++ src/request/repository/message-repository-stream.js
@@ -1,11 +1,32 @@
 'use strict';
 
 var glimpse = require('glimpse');
-var EventSource = require('event-source')
+var polyfill = require('event-source')
+
+var socket = (function() {
+    var connection;
+
+    var setup = function() {
+        connection = new polyfill.EventSource('/Glimpse/MessageStream');
+        connection.onmessage = function(e) {
+            if (!FAKE_SERVER) {
+                glimpse.emit('data.message.summary.found.stream', e.data);
+            }
+        };
+    }; 
+    
+    return {
+        check: function() {
+            if (!connection) {
+                setup();
+            }
+        }
+    };
+})();
 
 module.exports = {
     subscribeToLastestSummaries: function () {
-        // TODO: Setup SSE code
+        socket.check();
     },
     subscribeToDetailsFor: function (requestId) {
         // TODO: Setup SSE code
--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = {
             { test: /react/, loader: 'expose?React' },
             { test: /\.scss$/, loader: 'style!css!autoprefixer?browsers=last 2 version!sass?includePaths[]=' + (path.resolve(__dirname, './node_modules/bootstrap-sass/assets/stylesheets/')) },
             { test: /\.jsx$/, loader: 'jsx-loader?insertPragma=React.DOM' },
-            { test: /event-source/, loader: 'imports?this=>window!exports?EventSource'}
+            { test: /event-source/, loader: 'imports?this=>window!exports?EventSource=EventSource'}
         ]
     },
     plugins: [
