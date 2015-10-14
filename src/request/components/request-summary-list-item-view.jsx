fc8d5204075447f9d23a264c553f9b07cdb4918e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-13 22:33:22 -0700

    Latest round of updates to better display summary row

--- src/request/components/request-summary-list-item-view.jsx
+++ src/request/components/request-summary-list-item-view.jsx
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var glimpse = require('glimpse');
 var messageProcessor = require('../util/request-message-processor');
 var timeOrEmpty = glimpse.util.timeOrEmpty;
@@ -24,29 +25,58 @@ module.exports = React.createClass({
         var afterActionViewInvoked = payload.afterActionViewInvoked || {};
         var afterActionInvoked = payload.afterActionInvoked || {};
         var afterExecuteCommand = payload.afterExecuteCommand || [];
-        var abstract = {};
+        var abstract = { networkTime: 2, serverTime: 35, clientTime: 42 }; // TODO: temp until we get values 
+        
+        // should be pulled out into a different control
+        var queryGroup = '--';
+        if (afterExecuteCommand.length > 0) {
+            var queryCount = 0;
+            var queryDuration = 0;
+            _.forEach(afterExecuteCommand, function(command) {
+                queryCount++;
+                queryDuration += command.commandDuration;
+            });
+            
+            queryGroup = Math.round(queryDuration * 100) / 100 + 'ms / ' + queryCount;
+        }        
         
         return (
             <div className={containerClass} onClick={this.onSelect}>
-                <table className="list-item">
-                    <tr>
-                        <td width="90">{timeOrEmpty(endRequest.responseDuration)}</td>
-                        <td colSpan="6">
-                            {beginRequest.requestUrl} &nbsp; {beginRequest.requestMethod} &nbsp; {endRequest.responseStatusCode} ({endRequest.responseStatusText}) - {endRequest.responseContentType}
-                        </td>
-                        <td><Timeago time={beginRequest.requestStartTime} /></td>
-                    </tr>
-                    <tr>
-                        <td>{userIdentification.username}</td>
-                        <td>{timeOrEmpty(abstract.networkTime)}</td>
-                        <td>{timeOrEmpty(abstract.serverTime)}</td>
-                        <td>{timeOrEmpty(abstract.clientTime)}</td>
-                        <td>{afterActionInvoked.actionControllerName}.{afterActionInvoked.actionName}(...)</td>
-                        <td>{timeOrEmpty(afterActionInvoked.actionInvokedDuration)}</td>
-                        <td>{timeOrEmpty(afterActionViewInvoked.viewDuration)}</td>
-                        <td>{abstract.queryTime}ms / {abstract.queryCount}</td>
-                    </tr>
-                </table>
+                <div className="request-summary-data-holder">
+                    <div className="request-summary-leadGroup">
+                        <div>
+                            <div className="request-summary-responseDuration">{timeOrEmpty(endRequest.responseDuration)}</div>
+                            <div className="request-summary-duration-bar">
+                                <div className="request-summary-duration-bar-network"></div>
+                                <div className="request-summary-duration-bar-server"></div>
+                                <div className="request-summary-duration-bar-client"></div>
+                            </div>
+                        </div>
+                        <div className="request-summary-duration-items-outer">
+                            <div className="request-summary-duration-items">
+                                <div className="request-summary-networkTime">{timeOrEmpty(abstract.networkTime)}</div>
+                                <div className="request-summary-serverTime">{timeOrEmpty(abstract.serverTime)}</div>
+                                <div className="request-summary-clientTime">{timeOrEmpty(abstract.clientTime)}</div>
+                            </div>
+                        </div>
+                        <div className="request-summary-requestStartTime"><Timeago time={beginRequest.requestStartTime} /></div>
+                    </div>
+                    <div className="request-summary-detailGroup">
+                        <div className="request-summary-data-holder request-summary-detailMainGroup">
+                            <div className="request-summary-requestMethod">{beginRequest.requestMethod}</div>
+                            <div className="request-summary-requestUrl">{beginRequest.requestPath}<span className="request-summary-requestQueryString">{beginRequest.requestQueryString}</span></div>
+                            <div className="request-summary-responseStatusCode">{endRequest.responseStatusCode}<span className="request-summary-responseStatusText">{endRequest.responseStatusText}</span></div>
+                            <div className="request-summary-responseContentType">{endRequest.responseContentType}</div>
+                        </div>
+                        <div className="request-summary-data-holder request-summary-detailSubGroup">
+                            <div className="request-summary-actionGroup">{afterActionInvoked.actionControllerName}.{afterActionInvoked.actionName}(...)</div>
+                            <div className="request-summary-actionInvokedDuration">{timeOrEmpty(afterActionInvoked.actionInvokedDuration)}</div>
+                            <div className="request-summary-viewDuration">{timeOrEmpty(afterActionViewInvoked.viewDuration)}</div>
+                            <div className="request-summary-queryGroup">{queryGroup}</div>
+                            <div className="request-summary-username">{userIdentification.username}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     },
