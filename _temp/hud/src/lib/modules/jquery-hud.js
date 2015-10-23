6855e12a1591844535dc0b2561453ddbb5498274
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-23 16:33:16 -0700

    jQuery build needs an ajax transport

--- _temp/hud/src/lib/modules/jquery-hud.js
+++ _temp/hud/src/lib/modules/jquery-hud.js
@@ -6,6 +6,7 @@ require('jquery/src/event.js');
 require('jquery/src/event/alias.js');
 require('jquery/src/manipulation.js');
 require('jquery/src/ajax.js');
+require('jquery/src/ajax/xhr.js');
 require('jquery/src/core/parseHTML.js');
 
 module.exports = jQuery; 
