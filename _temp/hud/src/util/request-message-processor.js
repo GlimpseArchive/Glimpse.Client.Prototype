2f34d4a2bbc02a6af9ec95bc87cfc91e29100b76
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-23 16:35:17 -0700

    Update message processor to deal with list of messages rather than requests

--- _temp/hud/src/util/request-message-processor.js
+++ _temp/hud/src/util/request-message-processor.js
@@ -3,25 +3,25 @@
 var util = require('lib/util.js');
 
 module.exports = {
-	getTypeMessages: function(request, typeOptions) {
+	getTypeMessages: function(index, typeOptions) {
 		var result = {};
 		for (var key in typeOptions) {
 			var callback = typeOptions[key];
-			result[util.toCamelCase(key)] = callback(request, key)
+			result[util.toCamelCase(key)] = callback(index, key)
 		}
 		
 		return result;
 	},
-	getTypeMessageItem: function(request, type) {
-		var messageIds = request.types[type]; 
-		if (messageIds)	{
-			return request.messages[messageIds[0]].payload;
+	getTypeMessageItem: function(index, type) {
+		var messages = index[type]; 
+		if (messages)	{
+			return messages[0].payload;
 		}
 	},
-	getTypeMessageList: function(request, type) {
-		var messageIds = request.types[type]; 
-		if (messageIds)	{
-			return messageIds.map(function(id) { return request.messages[id].payload; });
+	getTypeMessageList: function(index, type) {
+		var messages = index[type]; 
+		if (messages)	{
+			return messages.map(function(message) { return message.payload; });
 		}
 	}
 };
