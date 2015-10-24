328ea527f016f7ea047200d15989e225356b123b
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-23 22:42:46 -0700

    Expose correct URLs that the server is expecting for the data endpoint and client

--- _temp/hud/src/lib/util.js
+++ _temp/hud/src/lib/util.js
@@ -1,7 +1,15 @@
 'use strict';
 
-var camelCaseRegEx = /^([A-Z])|[\s-_](\w)/g;
+var $ = require('$jquery');
 
+var camelCaseRegEx = /^([A-Z])|[\s-_](\w)/g;
+var currentRequestId = function() {
+    return document.getElementById('__glimpse_hud').getAttribute('data-request-id');
+};
+var usedMessageTypes = function() {
+    return 'environment,user-identification,end-request,begin-request,after-action-invoked,after-action-view-invoked,after-execute-command';
+}
+    
 module.exports = {
     localStorage: function (key, value) {
         if (arguments.length == 1) {
@@ -16,5 +24,11 @@ module.exports = {
             }
             return p1.toLowerCase();        
         });
+    },
+    resolveClientUrl: function() {
+        return '/glimpse/client/index.html?hash=bf90859f&requestId=' + currentRequestId();
+    },
+    resolveContextUrl: function() {
+        return '/glimpse/context/?contextId=' + currentRequestId() + '&types=' + usedMessageTypes();
     }
 };
