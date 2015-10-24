f81eee22f7c37cfde673afd7f526b249c66a95aa
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-23 22:46:15 -0700

    Hud fakes update to correct URL

--- _temp/hud/fake/fake-request-service.js
+++ _temp/hud/fake/fake-request-service.js
@@ -5,7 +5,7 @@ var chance = require('../../../fake/fake-extension');
 
 $.getJSON = function(url, data, callback) {
 	// TODO: turn into a strategy latere on if needed
-	if (url == '/glimpse/message-history') {
+	if (url == '/glimpse/context/?contextId=1234&types=environment,user-identification,end-request,begin-request,after-action-invoked,after-action-view-invoked,after-execute-command') {
 		setTimeout(function() {
 			callback(chance.mvcRequest(new Date()).messages);
 		}, 1000);
