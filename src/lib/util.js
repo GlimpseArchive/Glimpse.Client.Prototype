7337757b9759ad3cadb2386d7aef6b254adc970a
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-27 22:29:14 -0700

    Adds support for deep linking to a given request

--- src/lib/util.js
+++ src/lib/util.js
@@ -60,5 +60,17 @@ module.exports = {
     },
     timeOrEmpty: function(value) {
         return value !== null && value !== undefined ? value + 'ms' : '--';
+    },
+    getQueryStringParam: function(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (decodeURIComponent(pair[0]) == variable) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+        
+        return null;
     }
 };
--- src/request/request.js
+++ src/request/request.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var glimpse = require('glimpse');
+var util = require('lib/util');
 var requestRepository = require('./repository/request-repository');
 var userRepository = require('./repository/user-repository');
 
@@ -9,6 +10,12 @@ function initialize() {
     userRepository.triggerGetLastestUsers(); 
 
     glimpse.emit('shell.request.ready', {});
+    
+    // TODO: This should probably not be here
+    var requestId = util.getQueryStringParam('requestId');
+    if (requestId) {
+        glimpse.emit('shell.request.summary.selected', { requestId: requestId })
+    }
 }
 
 glimpse.on('shell.ready', initialize);
