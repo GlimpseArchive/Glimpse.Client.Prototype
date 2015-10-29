c13fa02042618b63c416983cb749e0ba8f1db8cc
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-28 22:17:05 -0700

    Fix bug where Navigation Timing info could be accessed to quickly and hence produce weird values

--- _temp/hud/src/index.js
+++ _temp/hud/src/index.js
@@ -19,21 +19,23 @@ var setup = state.current();
 
 // only load things when we have the data ready to go
 repository.getData(function(details) { 
-	// generate the html needed for the sections
-	var html = sections.render(details, setup);
-	html = '<div class="glimpse"><div class="glimpse-icon"></div><div class="glimpse-hud">' + html + '</div></div>';
-	
-	// insert the html into the dom
-	var holder = $(html).appendTo('body')
-	
-	// force the correct state from previous load
-	state.setup(holder);
-	
-	// setup events that we need to listen to
-	sections.postRender(holder);
-	
-	// TODO: need to find a better place for this
-	$('.glimpse-icon').click(function() {
-		window.open(util.resolveClientUrl(util.currentRequestId(), true), 'GlimpseClient');
-	});
+	$(function() { setTimeout(function() { 
+		// generate the html needed for the sections
+		var html = sections.render(details, setup);
+		html = '<div class="glimpse"><div class="glimpse-icon"></div><div class="glimpse-hud">' + html + '</div></div>';
+		
+		// insert the html into the dom
+		var holder = $(html).appendTo('body')
+		
+		// force the correct state from previous load
+		state.setup(holder);
+		
+		// setup events that we need to listen to
+		sections.postRender(holder);
+		
+		// TODO: need to find a better place for this
+		$('.glimpse-icon').click(function() {
+			window.open(util.resolveClientUrl(util.currentRequestId(), true), 'GlimpseClient');
+		});
+	}, 0); });
 });
