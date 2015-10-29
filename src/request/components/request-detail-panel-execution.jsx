351cf8e774cff47f3c4272eec85eef4a4fc2dbe6
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-29 11:58:22 -0700

    Add command to execution tab

--- src/request/components/request-detail-panel-execution.jsx
+++ src/request/components/request-detail-panel-execution.jsx
@@ -6,9 +6,8 @@ var _ = require('lodash');
 var React = require('react');
 var classNames = require('classnames');
 
-var getMessages = (function() {
+var getPayloads = (function() {
     var getItem = messageProcessor.getTypePayloadItem;
-    var getList = messageProcessor.getTypePayloadList;
     
     var options = {
         'end-request': getItem,
@@ -25,39 +24,61 @@ var getMessages = (function() {
     }
 })();
 
+var getMessages = (function() {
+    var getItem = messageProcessor.getTypeMessageItem;
+    var getList = messageProcessor.getTypeMessageList;
+    
+    var options = {
+        'before-action-invoked': getItem,
+        'after-action-invoked': getItem,
+        'before-execute-command': getList,
+        'after-execute-command': getList
+    };
+		
+    return function(request) {
+		return messageProcessor.getTypeStucture(request, options); 
+    }
+})();
+
 module.exports = React.createClass({
     render: function () {
         var request = this.props.request;
         
-        var payload = getMessages(request);
-        var beginRequestData = payload.beginRequest;
-        var routeData = payload.actionRoute;
-        var contentData = payload.actionContent;
-        var afterActionInvokedData = payload.afterActionInvoked;
-        var actionViewFoundData = payload.actionViewFound;
-        var afterActionViewInvokedData = payload.afterActionViewInvoked;
+        var payload = getPayloads(request);
+        var beginRequestPayload = payload.beginRequest;
+        var routePayload = payload.actionRoute;
+        var contentPayload = payload.actionContent;
+        var afterActionInvokedPayload = payload.afterActionInvoked;
+        var actionViewFoundPayload = payload.actionViewFound;
+        var afterActionViewInvokedPayload = payload.afterActionViewInvoked;
+        
+        var message = getMessages(request);
+        var beforeActionInvokedMessage = message.beforeActionInvoked;
+        var afterActionInvokedMessage = message.afterActionInvoked;
+        var beforeExecuteCommandMessages = message.beforeExecuteCommand;
+        var afterExecuteCommandMessages = message.afterExecuteCommand;
         
         var route = <div>No route found yet.</div>;
-        if (routeData) {
-            var routePath = beginRequestData ? (<div><span>{beginRequestData.path}</span><span>{beginRequestData.queryString}</span></div>) : '';
+        if (routePayload) {
+            var routePath = beginRequestPayload ? (<div><span>{beginRequestPayload.path}</span><span>{beginRequestPayload.queryString}</span></div>) : '';
         
             // process route
             route = (
                     <section className="tab-execution-item tab-execution-route"> 
                         <div className="tab-execution-title">Route</div>
-                        <div className="tab-execution-route-name tab-execution-important">{routeData.routeName}</div>
+                        <div className="tab-execution-route-name tab-execution-important">{routePayload.routeName}</div>
                         <div className="tab-execution-route-path">{routePath}</div>
-                        <div className="tab-execution-route-pattern">{routeData.routePattern}</div>
+                        <div className="tab-execution-route-pattern">{routePayload.routePattern}</div>
                     </section>
                 ); 
         }
         
         var action = <div>No action found yet.</div>;
-        if (afterActionInvokedData) {
+        if (afterActionInvokedPayload) {
             // process content
             var content;
-            if (contentData && contentData.binding) {
-                content = _.map(contentData.binding, function(item, i) {
+            if (contentPayload && contentPayload.binding) {
+                content = _.map(contentPayload.binding, function(item, i) {
                     return <li key={i}>{item.type} {item.name} {item.value}</li>;
                 });
                 
@@ -69,24 +90,64 @@ module.exports = React.createClass({
                     <section className="tab-execution-item tab-execution-action">
                         <div className="tab-execution-title">Action</div>
                         <div className="tab-execution-action-description tab-execution-important">
-                            {afterActionInvokedData.actionControllerName}.{afterActionInvokedData.actionName}({content})
+                            {afterActionInvokedPayload.actionControllerName}.{afterActionInvokedPayload.actionName}({content})
                         </div>
-                        <div className="tab-execution-timing">{afterActionInvokedData.actionInvokedDuration}ms</div>
+                        <div className="tab-execution-timing">{afterActionInvokedPayload.actionInvokedDuration}ms</div>
+                    </section>
+                ); 
+        }
+        
+        var commands = '';
+        if (beforeExecuteCommandMessages && afterExecuteCommandMessages && beforeActionInvokedMessage && afterActionInvokedMessage) {
+            var startIndex = beforeActionInvokedMessage.ordinal;
+            var endIndex = afterActionInvokedMessage.ordinal;
+            
+            beforeExecuteCommandMessages = beforeExecuteCommandMessages.sort(function(a, b) { return a.ordinal - b.ordinal; });
+            afterExecuteCommandMessages = afterExecuteCommandMessages.sort(function(a, b) { return a.ordinal - b.ordinal; });
+            
+            var commandItems = [];
+            for (var i = 0; i < beforeExecuteCommandMessages.length; i++) {
+                var beforeCommand = beforeExecuteCommandMessages[i];
+                var afterCommand = afterExecuteCommandMessages[i];
+                
+                if (beforeCommand.ordinal > startIndex && afterCommand.ordinal < endIndex) {
+                    var duration = '--'
+                    if (afterCommand.ordinal == beforeCommand.ordinal + 1) {
+                        duration = afterCommand.payload.commandDuration;
+                    }
+                    
+                    var commandItem = (
+                        <div key={beforeCommand.id} className="tab-execution-command-item">
+                            <div className="tab-execution-command-type">SQL</div>
+                            <div className="tab-execution-command-method">{beforeCommand.payload.commandMethod}</div>
+                            <div className="tab-execution-command-text">{beforeCommand.payload.commandText}</div>
+                            <div className="tab-execution-timing tab-execution-command-duration">{duration}ms</div>
+                        </div>
+                    );
+                    commandItems.push(commandItem);
+                }
+            }
+            
+            // process action
+            commands = (
+                    <section className="tab-execution-item tab-execution-command">
+                        <div className="tab-execution-title">Data</div>
+                        <div className="tab-execution-command-items">{commandItems}</div>
                     </section>
                 ); 
         }
         
         var view = <div>No view found yet.</div>;
-        if (afterActionViewInvokedData) {
+        if (afterActionViewInvokedPayload) {
             var viewTitle = null;
-            if (actionViewFoundData) { 
+            if (actionViewFoundPayload) { 
                 var viewTitleClass = classNames({
                     'tab-execution-important': true,
-                    'tab-execution-view-found': actionViewFoundData.viewDidFind,
-                    'tab-execution-view-notfound': !actionViewFoundData.viewDidFind
+                    'tab-execution-view-found': actionViewFoundPayload.viewDidFind,
+                    'tab-execution-view-notfound': !actionViewFoundPayload.viewDidFind
                 });
                 
-                viewTitle = <div><span className={viewTitleClass}>{actionViewFoundData.viewName}</span> - <span>{actionViewFoundData.viewPath}</span></div>;
+                viewTitle = <div><span className={viewTitleClass}>{actionViewFoundPayload.viewName}</span> - <span>{actionViewFoundPayload.viewPath}</span></div>;
             }
         
             // process action
@@ -94,7 +155,7 @@ module.exports = React.createClass({
                 <section className="tab-execution-item tab-execution-view">
                     <div className="tab-execution-title">View</div>
                     <div className="tab-execution-view-path">{viewTitle}</div>
-                    <div className="tab-execution-timing">{afterActionViewInvokedData.viewDuration}ms</div>
+                    <div className="tab-execution-timing">{afterActionViewInvokedPayload.viewDuration}ms</div>
                 </section>
             )
         }
@@ -102,7 +163,7 @@ module.exports = React.createClass({
         return (
             <div>
                 <div className="application-sub-item-header">Execution on Server</div>
-                {route}{action}{view}
+                {route}{action}{commands}{view}
             </div>
         );
     }
