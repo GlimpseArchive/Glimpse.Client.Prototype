a77be2b1b07cee2327f7360acf47890f8e76b708
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-11-11 11:45:05 -0800

    Updated to new loading logic

--- src/lib/components/loading.jsx
+++ src/lib/components/loading.jsx
@@ -2,33 +2,61 @@
 
 var React = require('react');
 
-var messages = [
-    'Loading...',
-    'Sorry won\'t be much longer...',
-    'Wow... we are taking a bit longer than usual...',
-    'Oops, looks like an error occured :|'
-];
+var messages = {
+    mandatory: (function() {
+        var listing = [
+                'Loading...',
+                'Sorry won\'t be much longer...',
+                'Wow... we are taking a bit longer than usual...',
+                'Oops, looks like an error occured :|'
+            ];
+        return {
+            length: listing.length,
+            process: function(next) {
+                return listing[next];
+            }
+        };
+    })(),
+    normal: (function() {
+        var init = 'Loading...';
+        
+        return {
+            length: 2,
+            process: function(next, props) {
+                return next === 0 ? init : props.message;
+            }
+        };
+    })()
+};
 
 module.exports = React.createClass({
-    getInitialState: function () {
+    getDefaultProps: function() {
+        return { mandatory: true };
+    },
+    getInitialState: function() {
         return { next: 0 };
     },
-    render: function () {
-        var text = messages[this.state.next];
+    render: function() {
+        var text = this.state.strategy.process(this.state.next, this.props);
 
-        return <span>{text}</span>;
+        return <div className="application-section-message">{text}</div>;
     },
-    componentDidMount: function () {
+    componentWillMount: function() {
+        var strategy = this.props.mandatory ? messages.mandatory : messages.normal;
+        
+        this.setState({ strategy: strategy })
+    },
+    componentDidMount: function() {
         this._update();
     },
-    _update: function () {
+    _update: function() {
         var that = this,
             next = that.state.next;
-        setTimeout(function () {
-            if (that.isMounted() && ++next < messages.length) {
+        setTimeout(function() {
+            if (that.isMounted() && ++next < that.state.strategy.length) {
                 that.setState({ next: next });
                 that._update();
             }
-        }, 1000 /*3000*/);
+        }, 3000);
     }
 });
